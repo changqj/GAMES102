5feed6f0aac6900e2246f0af7490a01b38e5923b
changqj <qingjun_chang@163.com>
2020-11-04 14:10:45 +0800

    hw4

--- homeworks/project/src/hw4/Components/CanvasData.h
+++ homeworks/project/src/hw4/Components/CanvasData.h
@@ -3,7 +3,7 @@
 #include <UGM/UGM.h>
 
 struct CanvasData {
-	std::vector<Ubpa::pointf2> points;
+	std::vector<Ubpa::pointf2> points;	// 型值点的具体坐标x-y
 	Ubpa::valf2 scrolling{ 0.f,0.f };
 	bool opt_enable_grid{ true };
 	bool opt_enable_context_menu{ true };
@@ -14,6 +14,13 @@ struct CanvasData {
 	bool exportData{ false };
 
 	bool isEnd{ false };
+
+	std::vector<float> leftDerivativeX;		// x(t_i)的左导数
+	std::vector<float> rightDerivativeX;	// x(t_i)的右导数
+	std::vector<float> leftDerivativeY;		// y(t_i)的左导数
+	std::vector<float> rightDerivativeY;	// y(t_i)的右导数
+
 };
+
 
 #include "details/CanvasData_AutoRefl.inl"
